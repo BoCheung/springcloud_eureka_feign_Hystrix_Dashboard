d9cd834b3e4ab9d55a8a8b74e5f5eabf023faf85
calledbo <calledbo@foxmail.com>
2021-03-20 22:35:28 +0800

    first commit

---
+++ microservice-hystrix-dashboard/src/test/java/com/example/cloud/microservicehystrixdashboard/MicroserviceHystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloud.microservicehystrixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceHystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
